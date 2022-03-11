13564c28d862361ff26f7ea6ce144cbea86b0843
A.R <87040192+AntoineRb@users.noreply.github.com>
2022-03-11 13:55:47 +0100

    Revert "Aerobi93 dev rectification bug"

--- backend/src/config.ts
+++
@@ -1,7 +0,0 @@
-import {IConfig} from './interface'
-
-const config: IConfig = {
-    port: process.env.PORT || "4000"
-};
-
-export default config;
--- src/service/user/read.ts
+++
@@ -1,9 +0,0 @@
-import Router from "koa-router";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export const findMany = async () => {
-    const allUsers = await prisma.user.findMany();
-    return allUsers;
-};
